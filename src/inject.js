db7c22f9b366df7e05c7cb6ffa50669d6296302a
jackspercy <jackspercy@gmail.com>
2016-12-28 19:16:44 +0000

    Remove Hashtag

--- src/inject.js
+++ src/inject.js
@@ -12,7 +12,7 @@ function main(){
 			return;
 		}
 		if ($('.chat-buttons-container').length) { //if button is there
-			$('a.button.button--icon-only.float-left').last().after('<a class="button button--icon-only float-left" id="increaseFont" title="Increase Font"><span class="ember-view"><figure class="icon"><svg class="svg-plus" height="16px" version="1.1" viewbox="0 0 16 16" width="16px" x="0px" y="0px">#<path d="M2,6.999h12v2H2V6.999z M7,1.999h2v12H7V1.999z"></path></svg></figure></span></a>');
+			$('a.button.button--icon-only.float-left').last().after('<a class="button button--icon-only float-left" id="increaseFont" title="Increase Font"><span class="ember-view"><figure class="icon"><svg class="svg-plus" height="16px" version="1.1" viewbox="0 0 16 16" width="16px" x="0px" y="0px"><path d="M2,6.999h12v2H2V6.999z M7,1.999h2v12H7V1.999z"></path></svg></figure></span></a>');
 			$('a.button.button--icon-only.float-left').last().after('<a class="button button--icon-only float-left" id="decreaseFont" title="Decrease Font"><span class="ember-view"><figure class="icon"><svg class="svg-minus" height="16px" version="1.1" viewbox="0 0 16 16" width="16px" x="0px" y="0px"><path d="M2,6.999h12v2H2V6.999z"></path></svg></figure></span></a>');//add new buttons with svg images
 			if ($('#change-chat-styles').length){
 				clearInterval(buttonExists);
